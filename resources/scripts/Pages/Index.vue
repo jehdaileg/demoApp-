72d7daa07b8b62089207ce2ff27c2cd347d02642
jehdaileg <jehdailegrand12@gmail.com>
2022-01-07 13:05:39 +0200

    Finish Inertia, Tailwindcss, vue js and vite installation

---
+++ resources/scripts/Pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+        <h5 class="text-4xl">Hello guys, installation done</h5>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mode: 'jit',
+	purge: [
+		'./resources/**/*.blade.php',
+		'./resources/**/*.{js,ts,vue}',
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
